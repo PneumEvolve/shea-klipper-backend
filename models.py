f8b0f528d2ae4ceebe26d2e13a24a4e3a2466c1d
Shea <sheaklipper@gmail.com>
2025-03-15 16:51:27 -0700

    Added datetime to main.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from database import Base  # Import Base model from database.py
 
 class User(Base):
