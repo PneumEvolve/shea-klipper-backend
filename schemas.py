c6dff02cb9d6d6f718124e2ef1767b346910e112
Shea <sheaklipper@gmail.com>
2025-03-15 19:04:03 -0700

    Changes schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from typing import List
 
 # User Schema
 class UserCreate(BaseModel):
@@ -22,3 +23,6 @@ class TranscriptionCreate(BaseModel):
 
     class Config:
         from_attributes = True  # ✅ Fix for Pydantic V2
+
+class FoodInventoryUpdateSchema(BaseModel):
+    items: List[str]
