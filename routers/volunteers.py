2340b5438d687aad405aaba9111fee1826829f84
Shea <sheaklipper@gmail.com>
2025-06-17 19:12:08 -0700

    Can volunteers submit request?

--- routers/volunteers.py
+++ routers/volunteers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models
 import schemas
-from ..database import get_db
+from database import get_db
 
 router = APIRouter(prefix="/volunteers", tags=["Volunteers"])
 
