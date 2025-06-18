1f92cb65561d7c99985c979485578fac2c48ae77
Shea <sheaklipper@gmail.com>
2025-06-17 19:00:30 -0700

    Can vols apply?

--- routers/volunteers.py
+++ routers/volunteers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from models import models, schemas
+import models
+import schemas
 from ..database import get_db
 
 router = APIRouter(prefix="/volunteers", tags=["Volunteers"])
