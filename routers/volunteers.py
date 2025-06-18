c98da3c955281c6a4ddd30c2d0550620ec2caebd
Shea <sheaklipper@gmail.com>
2025-06-17 18:54:40 -0700

    Can volunteer apply?

--- routers/volunteers.py
+++ routers/volunteers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .. import models, schemas
+from models import models, schemas
 from ..database import get_db
 
 router = APIRouter(prefix="/volunteers", tags=["Volunteers"])
